9d7c7f1c4484dbce68486df20d8336baca665c5a
BenWu <hidesor@yahoo.com.tw>
2017-03-10 23:59:06 +0800

    User加入seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,9 @@
 puts '建立預設使用者。' if User.first_or_create!([
   {
-    name: '系統管理員',
-    phone: '0921876501',
-    email: 'admin@adata.com',
+    name: 'Admin',
+    email: 'admin@gmail.com',
     password: '12345678',
     password_confirmation: '12345678',
-    role: 'admin',
-    is_active: true
-  },
-  {
-    name: '安心服務處管理員',
-    phone: '0921876501',
-    email: 'manager@adata.com',
-    password: '12345678',
-    password_confirmation: '12345678',
-    role: 'manager',
-    is_active: true
-  },
-  {
-    name: '消費者',
-    phone: '0921876703',
-    email: 'customer@adata.com',
-    password: '12345678',
-    password_confirmation: '12345678',
-    role: 'customer',
-    is_active: true
+    is_admin: true
   }
 ])
